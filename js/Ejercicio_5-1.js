376c6fc69b43dc3cc29f2158a05d606ffd000067
vitropez <64587299+vitropez@users.noreply.github.com>
2021-04-23 19:20:09 +0200

    23 Abril

--- js/Ejercicio_5-1.js
+++ js/Ejercicio_5-1.js
@@ -1,25 +1,6 @@
 // JavaScript Document
 
-/*function cambios() {
-    var imagen = document.getElementById("pato");
 
-         imagen.onmouseover = function () {
-          imagen.src = "img/Donald_2.png";
-
-}
-       imagen.onclick =function (){
-            imagen.src ="img/Goofi_1.png";
-
-}
-       imagen.onmouseout=function () {
-           imagen.src = "img/Donald_1.png";
-}
-       imagen.ondblclick=function (){
-
-            imagen.src = "img/Goofi_2.png";
-}
-
-}*/
 function cambio(Donald){
 
     Donald.src= "img/Donald_2.png";
--- js/Ejercicio_5-2.js
+++ js/Ejercicio_5-2.js
@@ -1,6 +1,27 @@
 // JavaScript Document
 
+function registraEventos(){
+    var imagen = document.getElementById("pato");
+   imagen.addEventListener("mouseover", Donald2);
+    imagen.addEventListener("click", Goofi1);
+    imagen.addEventListener("dblclick", Goofi2);
+    imagen.addEventListener("mouseout", Donald1);
+    function Donald2(){
+        imagen.src="img/Donald_2.png";
+    }
+    function Goofi1(){
+        imagen.src="img/Goofi_1.png";
+    }
+    function Goofi2(){
+        imagen.src="img/Goofi_2.png";
+    }
+    function Donald1(){
+        imagen.src="img/Donald_1.png";
+    }
 
+}
+
+window.addEventListener("load", registraEventos);
 
 
 
