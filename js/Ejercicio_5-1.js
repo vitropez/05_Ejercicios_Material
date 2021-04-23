24ecfaced78f39645a8fcdfda05c05213a2cbf78
vitropez <64587299+vitropez@users.noreply.github.com>
2021-04-23 14:53:53 +0200

    Initial commit

---
+++ js/Ejercicio_5-1.js
@@ -0,0 +1,54 @@
+// JavaScript Document
+
+/*function cambios() {
+    var imagen = document.getElementById("pato");
+
+         imagen.onmouseover = function () {
+          imagen.src = "img/Donald_2.png";
+
+}
+       imagen.onclick =function (){
+            imagen.src ="img/Goofi_1.png";
+
+}
+       imagen.onmouseout=function () {
+           imagen.src = "img/Donald_1.png";
+}
+       imagen.ondblclick=function (){
+
+            imagen.src = "img/Goofi_2.png";
+}
+
+}*/
+function cambio(Donald){
+
+    Donald.src= "img/Donald_2.png";
+}
+function cambio1(Goofy){
+
+    Goofy.src= "img/Goofi_1.png";
+}
+function cambio2(Goofy){
+
+    Goofy.src= "img/Goofi_2.png";
+}
+function cambio0(Donald){
+
+    Donald.src= "img/Donald_1.png";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
