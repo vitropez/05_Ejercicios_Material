d44ebb4bc0db9dcfbdbd2773f1dc614ede256a89
vitropez <64587299+vitropez@users.noreply.github.com>
2021-04-24 13:48:04 +0200

    24 Abril

--- js/Ejercicio_5-3.js
+++ js/Ejercicio_5-3.js
@@ -1,4 +1,32 @@
 // JavaScript Document
+function comprobar() {
+    var longitud = document.getElementById("textoDNI").value.length;
+    if (longitud===8){
+        escribirLetra();
+        cambiarTexto();
+        cambiarColorFondo();
+
+
+    }
+    function escribirLetra(){
+
+        let letras ="TRWAGMYFPDXBNJZSQVHLCKET";
+        let tarjeta=document.getElementById("textoDNI")
+        let DNI=tarjeta.value
+        let  posicion = DNI % 23;
+        let  letra = letras.charAt(posicion);
+        tarjeta.value=( DNI+"-"+letra);
+
+    }
+    function  cambiarTexto(){
+        document.getElementById("capaTextoId").innerHTML="DNI completo";
+
+    }
+    function cambiarColorFondo(){
+        document.getElementById("form").style.background="aquamarine";
+    }
+}
+
 
 
 
